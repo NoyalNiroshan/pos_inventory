56ecaeed2f6fa6f494cf2efa348421410ee94fda
jothikaN <jothikajo01@gmail.com>
2024-09-18 13:08:23 +0530

    permission crud

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,13 @@
 <?php
 
+use App\Http\Controllers\BrandController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PermissionController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::resource('permissions',PermissionController::class);
-
+Route::resource('brands',BrandController::class);
 
 Route::get('/', function () {
     return view('welcome');
